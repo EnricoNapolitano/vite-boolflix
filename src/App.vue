92faa8300a3be302815af6681ae3fabb72e54d5d
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-26 23:54:51 +0100

    header with search bar

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios'
+import { store } from './data/store';
+import { uri } from './data'
+import AppHeader from './components/AppHeader.vue';
+export default {
+    name: 'App',
+    data() { return { store } },
+    components: { AppHeader },
+    methods: {
+        showSearch(par) {
+            //API's call
+            axios.get(`${uri.base}${uri.sm}?api_key=${uri.apiKey}&query=${par}`)
+                .then((res) => {
+                    store.movies = res.data.results;
+                });
+            axios.get(`${uri.base}${uri.sTv}?api_key=${uri.apiKey}&query=${par}`)
+                .then((res) => {
+                    store.tvShows = res.data.results;
+                });
+        }
+    }
+}
+</script>
+
+<template>
+    <app-header @send="showSearch"></app-header>
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() { return { value: '' } },
+    emits: ['send']
+}
+</script>
+<template>
+    <header>
+        <h1>BOOLFLIX</h1>
+        <form @submit.prevent="$emit('send', value)">
+            <input type="text" v-model="value">
+            <button>SEARCH</button>
+        </form>
+    </header>
+</template>
+<style>
+header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+}
+</style>
