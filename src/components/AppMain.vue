fcd2466e684c8c4155989244c6b5e82367af8fd8
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-30 00:06:05 +0100

    PH, MS and background color to AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,27 @@
 <script>
-import MediaCard from './MediaCard.vue';
+import PlaceHolder from './PlaceHolder.vue';
+import MediaSections from './MediaSections.vue'
 export default {
+    name: 'AppMain',
     props: { movies: Array, tvShows: Array },
-    components: { MediaCard }
+    components: { PlaceHolder, MediaSections },
 }
 </script>
 <template>
-    <div v-if="!movies && !tvShows">
-        Cerca un titolo
-    </div>
-    <section v-else class="medias-section">
-        <h2>MOVIES</h2>
-        <media-card v-for="movie in movies" :item="movie"></media-card>
-        <hr>
-        <h2>SERIES</h2>
-        <media-card v-for="tvShow in tvShows" :item="tvShow"></media-card>
-    </section>
+    <main>
+        <div class="container">
+            <place-holder v-if="!movies && !tvShows"></place-holder>
+            <media-sections v-else :movies="movies" :tvShows="tvShows"></media-sections>
+        </div>
+    </main>
 </template>
+
+<style lang="scss">
+@use '../assets/scss/partials/variables' as*;
+
+main {
+    background-color: $black;
+    min-height: calc(100vh - 55px);
+}
+</style>
+
