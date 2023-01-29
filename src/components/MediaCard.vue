327f62d786fa95e5d0fdcb1881c7f2a2553370dc
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-30 00:02:46 +0100

    base image movie/shows changed size

--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -1,5 +1,6 @@
 <script>
 export default {
+    name: 'MediaCard',
     props: {
         item: { type: Object },
     },
@@ -32,7 +33,7 @@ export default {
         //*** BACKGROUND IMAGE FOR MOVIE/SHOW ***
         backdropImg() {
             //building URL image
-            const baseImgUrl = 'https://image.tmdb.org/t/p/w500';
+            const baseImgUrl = 'https://image.tmdb.org/t/p/w342';
             return baseImgUrl + this.item.backdrop_path
         },
 
@@ -48,7 +49,7 @@ export default {
         numEmptyStars() {
             return Math.ceil(4 - (this.rating / 10))
         }
-    },
+    }
 }
 </script>
 
@@ -56,21 +57,37 @@ export default {
     <article>
         <figure>
             <img :src="backdropImg" alt="backdrop image">
+            <div class="overlay">
+                <h3 v-if="isSameTitle">{{ title }}</h3>
+                <div v-else>
+                    <h3>{{ title }}</h3>
+                    <h4>{{ originalTitle }}</h4>
+                </div>
+                <div class="language">
+                    <p v-if="!hasFlag">{{ item.original_language }}</p>
+                    <img class="flag" v-else :src="flagImgPath" :alt="item.original_language">
+                </div>
+                <div class="rating">
+                    <i v-for="star in numFullStars" class="fa-solid fa-star"></i>
+                    <i v-for="star in numEmptyStars" class="fa-regular fa-star"></i>
+                </div>
+                <p>{{ item.overview }}</p>
+            </div>
         </figure>
-        <h3 v-if="isSameTitle">{{ title }}</h3>
-        <div v-else>
-            <h3>{{ title }}</h3>
-            <h4>{{ originalTitle }}</h4>
-        </div>
-        <div class="language">
-            <p v-if="!hasFlag">{{ item.original_language }}</p>
-            <img v-else :src="flagImgPath" :alt="item.original_language">
-        </div>
-        <div class="rating">
-            <i v-for="star in numFullStars" class="fa-solid fa-star"></i>
-            <i v-for="star in numEmptyStars" class="fa-regular fa-star"></i>
-        </div>
-        <p>{{ item.overview }}</p>
 
     </article>
 </template>
+
+<style lang="scss">
+@use '../assets/scss/partials/variables' as*;
+
+article {
+    color: $light-grey;
+    display: flex;
+}
+
+.flag {
+    width: 30px;
+    height: auto;
+}
+</style>
