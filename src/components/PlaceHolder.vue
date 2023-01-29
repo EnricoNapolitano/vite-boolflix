fec0959c08b2e56358f57fccb2a13a041e1eaadc
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-30 00:04:26 +0100

    Created component PlaceHolder

---
+++ src/components/PlaceHolder.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="placeholder">
+        <figure>
+            <img src="../assets/img/popcorn.png" alt="popcorn">
+        </figure>
+        <div>
+            <span>Pronti i popcorn?</span>
+            <div>Inizia la tua ricerca</div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@use '../assets/scss/partials/variables' as*;
+
+.placeholder {
+    padding-top: 100px;
+    text-align: left;
+
+    display: flex;
+    align-items: center;
+
+    figure {
+        padding-right: 50px;
+        width: 410px;
+
+        img {
+            width: 100%;
+        }
+    }
+
+    span {
+        color: $light-grey;
+        font-size: 9rem;
+        font-weight: 900;
+        line-height: 80px;
+    }
+
+    div {
+        padding-top: 24px;
+        color: white;
+        font-size: 5rem;
+        font-weight: 900;
+    }
+}
+</style>
