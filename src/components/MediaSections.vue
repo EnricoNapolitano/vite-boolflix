831e1130ba4cd5b060748e92ea5d5caa36ec6492
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-30 00:04:00 +0100

    Created MediaSections component

---
+++ src/components/MediaSections.vue
@@ -0,0 +1,18 @@
+<script>
+import MediaCard from './MediaCard.vue';
+export default {
+    name: 'MediaSections',
+    props: { movies: Array, tvShows: Array },
+    components: { MediaCard }
+}
+</script>
+
+<template>
+    <div class="medias-section">
+        <h2>MOVIES</h2>
+        <media-card v-for="movie in movies" :item="movie"></media-card>
+        <hr>
+        <h2>SERIES</h2>
+        <media-card v-for="tvShow in tvShows" :item="tvShow"></media-card>
+    </div>
+</template>
