6ed91752df5648b79e0bd113c639d68bee234ba0
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-29 18:42:05 +0100

    rating system and backdrop image

--- src/components/MediaCard.vue
+++ src/components/MediaCard.vue
@@ -10,6 +10,9 @@ export default {
         originalTitle() {
             return this.item.original_title || this.item.original_name
         },
+        isSameTitle() {
+            return this.title === this.originalTitle
+        },
         hasFlag() {
             const lang = ['it', 'en'];
             return lang.includes(this.item.original_language)
@@ -18,19 +21,44 @@ export default {
             //building the image path
             const img = this.item.original_language;
             return new URL(`../assets/img/${img}.png`, import.meta.url).href
+        },
+        backdropImg() {
+            //building URL image
+            const baseImgUrl = 'https://image.tmdb.org/t/p/w500';
+            return baseImgUrl + this.item.backdrop_path
+        },
+        rating() {
+            return this.item.vote_average * 5
+        },
+        numFullStars() {
+            return Math.ceil(this.rating / 10)
+        },
+        numEmptyStars() {
+            return Math.ceil(4 - (this.rating / 10))
         }
-    }
+    },
 }
 </script>
 
 <template>
     <article>
-        <h3>{{ title }}</h3>
-        <h4>{{ originalTitle }}</h4>
-        <div>
+        <figure>
+            <img :src="backdropImg" alt="backdrop image">
+        </figure>
+        <h3 v-if="isSameTitle">{{ title }}</h3>
+        <div v-else>
+            <h3>{{ title }}</h3>
+            <h4>{{ originalTitle }}</h4>
+        </div>
+        <div class="language">
             <p v-if="!hasFlag">{{ item.original_language }}</p>
             <img v-else :src="flagImgPath" :alt="item.original_language">
         </div>
-        <p>{{ item.vote_average }}</p>
+        <div class="rating">
+            <i v-for="star in numFullStars" class="fa-solid fa-star"></i>
+            <i v-for="star in numEmptyStars" class="fa-regular fa-star"></i>
+        </div>
+        <p>{{ item.overview }}</p>
+
     </article>
 </template>
