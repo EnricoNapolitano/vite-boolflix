9a5902140092f2704dc5ba9f0f5cc487776636e0
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-30 12:30:59 +0100

    media section's style

--- src/components/MediaSections.vue
+++ src/components/MediaSections.vue
@@ -9,10 +9,37 @@ export default {
 
 <template>
     <div class="medias-section">
-        <h2>MOVIES</h2>
-        <media-card v-for="movie in movies" :item="movie"></media-card>
-        <hr>
-        <h2>SERIES</h2>
-        <media-card v-for="tvShow in tvShows" :item="tvShow"></media-card>
+        <h2 class="wrap">FILM</h2>
+        <div class="media-card">
+            <media-card v-for="movie in movies" :item="movie"></media-card>
+        </div>
+        <hr class="wrap">
+        <h2 class="wrap">SERIE</h2>
+        <div class="media-card">
+            <media-card v-for="tvShow in tvShows" :item="tvShow"></media-card>
+        </div>
     </div>
 </template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as*;
+
+hr {
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
+
+h2 {
+    font-size: 4rem;
+    padding: 30px 0;
+
+    color: $light-grey;
+    text-shadow: 2px 2px 5px black;
+}
+
+.media-card {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+</style>
