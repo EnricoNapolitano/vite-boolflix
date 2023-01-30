091837d3a32d9c39bc0feea3b01fee1c2ff0b560
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-30 14:24:42 +0100

    font awesom star icons installed by npm

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import "@fontsource/questrial" //imported font questrial (simil to helvetica)
+import { library } from '@fortawesome/fontawesome-svg-core' // FA library
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons' // FA icon
+import { faStar as solidStar } from '@fortawesome/free-solid-svg-icons' // FA icon
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome' //FA component
 import App from './App.vue'
 
-createApp(App).mount('#app')
+library.add(regularStar, solidStar)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.mount('#app')
